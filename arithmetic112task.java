13ebfd624dd66d18e5d741c5505723c5ab6830f7
Karl-B07 <karldavidbbron5@gmail.com>
2024-10-08 08:39:52 +0000

    ArithmeticTask112

--- arithmetic112task.java
+++ arithmetic112task.java
@@ -1,16 +1,20 @@
-public class ArithmeticTask{
+public class arithmetic112task {
     public static void main(String[] args){
-
         int result = 1 + 2;
                 System.out.println(result);
-        result -= 1;
+
+        result -=1;
                 System.out.println(result);
+
         result *= 2;
                 System.out.println(result);
+
         result /= 2;
                 System.out.println(result);
+
         result += 8;
-                System.out.println(result);                                                                         
+                System.out.println(result);
+                                                                                        
         result %= 7;
                 System.out.println(result);
     }
