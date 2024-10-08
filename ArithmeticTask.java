cac32448456f1abf4a9f235b41584dc5f8119b7e
Karl-B07 <karldavidbbron5@gmail.com>
2024-10-08 08:10:41 +0000

    ArithmeticTask12

--- ArithmeticTask.java
+++ ArithmeticTask.java
@@ -3,19 +3,14 @@ public class ArithmeticTask{
 
         int result = 1 + 2;
                 System.out.println(result);
-
         result -= 1;
                 System.out.println(result);
-
         result *= 2;
                 System.out.println(result);
-
         result /= 2;
                 System.out.println(result);
-
         result += 8;
-                System.out.println(result);
-                                                                                        
+                System.out.println(result);                                                                         
         result %= 7;
                 System.out.println(result);
     }
