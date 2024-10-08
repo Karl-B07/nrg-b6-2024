be255e5389dce10b38342a1d4ba90cc2e568a757
Karl-B07 <karldavidbbron5@gmail.com>
2024-10-08 09:55:32 +0000

    Tasksheet113

---
+++ Tasksheet113.java
@@ -0,0 +1,13 @@
+public class Tasksheet113 {
+    public static void main(String[] args){
+        byte zero = 0;
+        short one = 110;
+        boolean t = true;
+        float two = (float) 2.0;
+        int won = 1;
+        int three = 3;
+
+        String output = "H" + three + one  + " " + "w" + zero + "r" + won + "d " + two + " " + t;
+        System.out.println(output);
+    }
+}
--- arithmetic112task.java
+++ arithmetic112task.java
@@ -1,5 +1,6 @@
 public class arithmetic112task {
     public static void main(String[] args){
+        
         int result = 1 + 2;
                 System.out.println(result);
 
