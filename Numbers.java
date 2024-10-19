ea57383fa6e3a11684e9ece09b2b9bf3919dc929
Karl-B07 <karldavidbbron5@gmail.com>
2024-10-19 12:59:19 +0000

    Task119

---
+++ Numbers.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Numbers {
+	public int addNumbers(int a, int b) {
+                return a + b;  
+        }
+        public int subtractNumbers(int a, int b){
+                return a - b;
+        }
+        public int multiplyNumbers(int a, int b){
+                return a * b;
+        }
+        public int divideNumbers(int a,int b){
+                return a / b;
+        }
+        public static void main(String[] args){
+                Scanner input = new Scanner(System.in);
+                System.out.println("input two number");
+                  int a = input.nextInt();
+                  int b = input.nextInt();
+                Numbers numbers = new Numbers();
+
+int add = numbers.addNumbers(a, b);
+int subtract = numbers.subtractNumbers(a, b);
+int multiply = numbers.multiplyNumbers(a, b);
+int divide = numbers.divideNumbers(a, b);
+
+
+//   String c = "difference";
+// int result = a - b;
+//int resultsum = a + b;
+// double result1 = a / b;
+System.out.println();
+System.out.println("Sum " + add);
+System.out.println("Difference "+ subtract);
+System.out.println("Product "+ multiply);
+        System.out.println("Qoutient "+ divide);
+    }
+}
